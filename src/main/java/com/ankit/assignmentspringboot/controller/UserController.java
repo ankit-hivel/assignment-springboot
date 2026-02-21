bc59f87e963d8e64a4ae8e1616a3a24ab7003126
Ankit Ydv <ankit@hivel.ai>
2026-02-21 22:07:48 +0530

    json response

--- src/main/java/com/ankit/assignmentspringboot/controller/UserController.java
+++ src/main/java/com/ankit/assignmentspringboot/controller/UserController.java
@@ -2,14 +2,18 @@ package com.ankit.assignmentspringboot.controller;
 
 import com.ankit.assignmentspringboot.model.UserModel;
 import com.ankit.assignmentspringboot.service.UserService;
+import com.ankit.assignmentspringboot.utility.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.autoconfigure.JacksonProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/")
-public class UserController {
+public class UserController extends Exception{
 
     private final UserService userService;
 
@@ -19,22 +23,22 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> saveUser(@RequestBody UserModel user) {
-        System.out.println(user);
-        userService.saveUserData(user);
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<ApiResponse> saveUser(@RequestBody UserModel user) {
+        try {
+            System.out.println(user);
+            userService.saveUserData(user);
+            ApiResponse resp = new ApiResponse(true, "saved user", null);
+            return ResponseEntity.ok(resp);
+        }catch (Exception e){
+            ApiResponse resp = new ApiResponse(false, "failed to save user", null);
+            return ResponseEntity.ok(resp);
+        }
     }
 
     @GetMapping
-    public ResponseEntity<HttpStatus> getUserById(@RequestParam int id) {
-        userService.getUserById(id);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
-
-    @GetMapping
-    public ResponseEntity<HttpStatus> getUserByEmail(@RequestParam String email){
-        System.out.println("email: " + email);
-        userService.getUserByEmail(email);
-        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse> getUserByIdOrEmail(@RequestParam int id, @RequestParam String email) {
+        Optional<UserModel> user = (email != null) ? userService.getUserByEmail(email): userService.getUserById(id);
+        ApiResponse resp = new ApiResponse(true, "user found", user);
+        return ResponseEntity.ok(resp);
     }
 }
