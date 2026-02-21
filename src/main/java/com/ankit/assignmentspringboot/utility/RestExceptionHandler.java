195b224bef2b9a4f11141c02d3f7c63a09b7480c
Ankit Ydv <ankit@hivel.ai>
2026-02-21 22:07:21 +0530

    global exception handler

---
+++ src/main/java/com/ankit/assignmentspringboot/utility/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.ankit.assignmentspringboot.utility;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleGlobalException(Exception e) {
+        System.out.println(e);
+        ApiResponse resp = new ApiResponse();
+        resp.setStatus(false);
+        resp.setMessage("Internal Server Error");
+        return ResponseEntity.ok(resp);
+    }
+}
